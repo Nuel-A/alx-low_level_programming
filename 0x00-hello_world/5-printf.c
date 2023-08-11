a649e0933fc0399cb27fe184279f6e5de4d366f2
Nuel-A <nuelayeni365@gmail.com>
2023-08-11 09:49:55 -0700

    proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - This will print a statement
+ *Return: success 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
