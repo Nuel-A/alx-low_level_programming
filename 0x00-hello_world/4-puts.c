a36d80fa5ea02627c36702b3a6f5ab76ffce16c3
Nuel-A <nuelayeni365@gmail.com>
2023-08-11 09:00:56 -0700

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - this is a program that prints a string
+ *
+ * Return: always 0 success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
