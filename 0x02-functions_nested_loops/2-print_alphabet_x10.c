2547bc9bb775d149c5fa905962cccee26003b88e
Nuel-A <nuelayeni365@gmail.com>
2023-08-18 08:02:07 -0700

    2-print_alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10 - This program prints the alphabet 10x
+ * in lowercase.
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char e;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (e = 'a'; e <= 'z'; e++)
+			_putchar(e);
+
+		_putchar('\n');
+	}
+
+}
